99b5840c0965ce4bea3fa91af4cb260eb5140e8f
ZhaioXin <zx62020951@163.com>
2020-07-22 10:32:49 +0800

    init

---
+++ flutter_service_worker.js
@@ -0,0 +1,44 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "assets/AssetManifest.json": "0d266ffbe90dae02458487c9d33b7373",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/LICENSE": "e714ced019fd108b460c9b9f4cc92b7d",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/packages/flutter_markdown/assets/logo.png": "67642a0b80f3d50277c44cde8f450e50",
+"assets/quote.png": "4da61e6a25cc69bf7fa7b7f338f375bf",
+"assets/web/assets/quote.png": "4da61e6a25cc69bf7fa7b7f338f375bf",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"index.html": "69041aa2d68195a01742f40da00c2317",
+"/": "69041aa2d68195a01742f40da00c2317",
+"loading.gif": "37820c5ac70ccb54498ee2d87a404e20",
+"main.dart.js": "a1cac2c752059f84f4ec79b2e0069f89",
+"manifest.json": "6c8c9b978b65baa657f380f6b03b6812"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
